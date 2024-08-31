9615eaca2263345047c92e6d7774790317c4b406
Wangdra <dwangdrashers@gmail.com>
2024-09-01 04:18:04 +0600

    first commit

---
+++ JS/main.js
@@ -0,0 +1,104 @@
+/*=============== toggle icon navbar ===================*/
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+};
+
+/*=============== scroll section active link ===================*/
+
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  let top = window.scrollY;
+
+  sections.forEach((sec) => {
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  /*=============== sticky navbar ===================*/
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /*=============== remove toggle icon and navbar  ===================*/
+  menuIcon.classList.remove("fa-xmark");
+  navbar.classList.remove("active");
+};
+
+/*=============== scroll reveal ===================*/
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "button" }
+);
+ScrollReveal().reveal(".home-contact h1, .about-image", { origin: "left" });
+ScrollReveal().reveal(".home-contact p, .about-content", { origin: "right" });
+
+/*=============== typed js ===================*/
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Frontend Developer",
+    "Backend Developer",
+    "Web Designer",
+    "Cyber Security(PenTester)",
+  ],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
+
+const typed2 = new Typed(".multiple-text-2", {
+  strings: [
+    "Frontend Developer",
+    "Backend Developer",
+    "Web Designer",
+    "Cyber Security(PenTester)",
+  ],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
+
+/*=============== toggle readme btn ===================*/
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector(".read-more-btn")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      const moreContent = document.querySelector(".more-strengths");
+
+      if (
+        moreContent.style.display === "none" ||
+        moreContent.style.display === ""
+      ) {
+        moreContent.style.display = "block";
+        this.textContent = "Read less";
+      } else {
+        moreContent.style.display = "none";
+        this.textContent = "Read more";
+      }
+    });
+});
